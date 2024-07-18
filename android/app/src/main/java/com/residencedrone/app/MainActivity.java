206187cd968ef61af085e4be59d4e786ec19ec8a
Dave <david.bethke@gmail.com>
2024-07-18 13:50:06 -0500

    initial commit 2

---
+++ android/app/src/main/java/com/residencedrone/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.residencedrone.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.residencedrone.app',
+  appName: 'test5',
+  webDir: 'dist/test5/browser'
+};
+
+export default config;
